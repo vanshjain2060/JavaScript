74c5a1a77a0d28f5b89e7380e233a62f62e529d1
vanshjain2060 <128175824+vanshjain2060@users.noreply.github.com>
2024-03-20 15:12:24 +0000

    check

---
+++ 01_basics/variables.js
@@ -0,0 +1,4 @@
+const accountId = 6;
+// const is a kind of variable which can be initialized only once and it can't be over ride 
+let name = vansh ;
+// let is a nornal valrable which can be over ride again we it needed
